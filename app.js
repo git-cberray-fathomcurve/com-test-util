86437c9ce343f745bbb60e97d82f232791b6f4ba
git-cberray-fathomcurve <91254742+git-cberray-fathomcurve@users.noreply.github.com>
2021-09-29 15:20:52 -0500

    Create app.js
    added basic app

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const os = require('os');
+
+const params = {
+    hostname: os.hostname(),
+    username: os.userInfo().username,
+    os: os.platform(),
+    release: os.release(),
+    freemem: os.freemem()/1000000 + ' mb',
+    totalmem: os.totalmem()/1000000 + ' mb',
+    dirname: __dirname,
+}
+
+console.log('System Parameters: ',params);
+
